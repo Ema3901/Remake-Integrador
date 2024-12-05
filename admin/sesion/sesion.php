0139a91f7a649bc3dff3e5d7675d904b2cecb380
Ema3901 <ENEma3901@gmail.com>
2024-12-05 17:30:49 -0600

    Update sesion.php

--- admin/sesion/sesion.php
+++ admin/sesion/sesion.php
@@ -69,8 +69,8 @@ index.php');
     <!-- Bootstrap 5.3 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="/src/css/style.css">
-    <link rel="stylesheet" href="/src/css/auth.css">
+    <link rel="stylesheet" href="https://calzadojj.net/src/css/style.css">
+    <link rel="stylesheet" href="https://calzadojj.net/src/css/auth.css">
 </head>
 <body class="auth-page">
     <!-- Header -->
