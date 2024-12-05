aedb437d2c0db8a5decaa23f5ddcca7c00397e16
Ema3901 <ENEma3901@gmail.com>
2024-12-05 15:33:23 -0600

    Update cuentas.php

--- admin/usuarios/cuentas.php
+++ admin/usuarios/cuentas.php
@@ -128,10 +128,9 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </div>
     </div>
-                </main>
-
         <!-- Footer -->
         <?php include __DIR__ . '/../src/footer.php'; ?>
+    </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script>
@@ -307,5 +306,6 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
             setupRefreshTable();
         });
     </script>
+    
 </body>
 </html>
