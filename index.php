62c92bf828778e7ab4c2f447c3f097892174322e
Ema3901 <ENEma3901@gmail.com>
2024-12-06 14:59:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,11 @@
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/src/css/style.css">
+    <link rel="stylesheet" href="/src/css/footer.css">
+    <link rel="stylesheet" href="/src/css/carrusel.css">
+    <link rel="stylesheet" href="/src/css/categorias.css">
 
 </head>
 <body>
