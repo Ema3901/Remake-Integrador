d6854fcd3e3d5c54c9f0dced57c49e349a0560fa
Ema3901 <ENEma3901@gmail.com>
2024-12-05 18:02:48 -0600

    Update cuentas.php

--- admin/usuarios/cuentas.php
+++ admin/usuarios/cuentas.php
@@ -1,5 +1,15 @@
 <?php
+// Incluir el archivo de conexión
 include __DIR__ . '/../../src/database/db.php';
+
+session_start();
+
+// Si no hay una sesión activa, redirigir a /sesion/sesion.php
+if (!isset($_SESSION['user_id'])) {
+    header('Location: /sesion/sesion.php');  // Cambia esto por la URL de tu página de sesión
+    exit();
+}
+
 
 // Llamar al procedimiento almacenado
 $sql = "CALL GetUsersWithRanges()";
