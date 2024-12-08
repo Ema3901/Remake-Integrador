f5ebb5d034884c6288f53f441a4f6247d308a22a
Ema3901 <ENEma3901@gmail.com>
2024-12-07 23:46:24 -0600

    Update detalleProducto.php

--- store/detalleProducto.php
+++ store/detalleProducto.php
@@ -165,7 +165,6 @@ $stmt_recomendados->closeCursor();
     <?php include __DIR__ . '/../src/include/footer.php'; ?>
 
     <!-- Font Awesome Integration -->
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 </body>
