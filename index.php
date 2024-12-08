ac63a177abea35b51a2110e92cd13e1095d99f14
Ema3901 <ENEma3901@gmail.com>
2024-12-07 23:44:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -108,9 +108,6 @@
     <?php include __DIR__ . '/src/include/footer.php'; ?>
 
 
-<!-- Font Awesome Integration -->
-<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-
 <!-- Scripts de Bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
