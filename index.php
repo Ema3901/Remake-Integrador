04fefb5bb1a20cfbc5a9ed59bfb8dde87e10dc58
Ema3901 <ENEma3901@gmail.com>
2024-12-09 00:27:14 -0600

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <a href="/store/detalleProducto.php?id=42">
+            <a href="/store/detalleProducto.php?id=0">
                 <img src="/src/images/index/carrusel/car1.png" class="d-block w-100" alt="Ofertas de Calzado JJ">
             </a>
         </div>
