81de7d3d2de1ebd20269c00428d00bda879cfca2
Ema3901 <ENEma3901@gmail.com>
2024-12-09 23:36:32 -0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -34,6 +34,7 @@ try {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://calzadojj.net/src/css/style.css">
+    <link rel="stylesheet" href="https://calzadojj.net/src/css/footer.css">
 </head>
 <body>
 
